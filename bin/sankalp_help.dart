8660949bd93b9a7d885201bb97c56c2e253cff79
Rachit Goyal <rg410345@gmail.com>
2024-01-24 21:41:02 +0530

    Initial commit

---
+++ bin/sankalp_help.dart
@@ -0,0 +1,5 @@
+import 'package:sankalp_help/sankalp_help.dart' as sankalp_help;
+
+void main(List<String> arguments) {
+  print('Hello world: ${sankalp_help.calculate()}!');
+}
---
+++ test/sankalp_help_test.dart
@@ -0,0 +1,8 @@
+import 'package:sankalp_help/sankalp_help.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
